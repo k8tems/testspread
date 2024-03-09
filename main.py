0f35d88563da360553400b01cb731632b5c557e6
k8tem <k8tems@live.com>
2024-03-09 22:49:16 +0900

    Rename title of sheet and change order of parameters passed to update to mitigate deprecation warning

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import gspread
 
 
 if __name__ == '__main__':
-    SHEET_NAME = 'StableCascade損失計算シート'
+    SHEET_NAME = 'StableCascade Loss Vis'
     scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
     client = gspread.service_account()
     sheet = client.open(SHEET_NAME).sheet1
     row = 1
     values = ['Cell 1', 'Cell 2']
-    sheet.update(f'A{row}:B{row}', [values])
+    sheet.update([values], f'A{row}:B{row}')
