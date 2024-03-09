69f8378a9694e84597cc82a68d07525269ec8ea9
k8tem <k8tems@live.com>
2024-03-10 00:12:57 +0900

    Dumbass me forgot to add_image

--- pyxl.py
+++ pyxl.py
@@ -15,6 +15,7 @@ def write_img(ws, fname, x, y):
         pos=XDRPoint2D(p2e(x), p2e(y)),
         ext=XDRPositiveSize2D(p2e(img.width), p2e(img.height))
     )
+    ws.add_image(img)
 
 
 def write_row(ws, row, data, start_col=1):
